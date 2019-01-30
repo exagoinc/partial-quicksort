cfef40ccb1fd05efa9d279041fec99c70d6b869c
Peter Fernandes <supersonicandtails@gmail.com>
2019-01-30 15:23:08 -0500

    Initial commit

---
+++ partial-quicksort.ts
@@ -0,0 +1,39 @@
+private static PartialQuickSort<T>(a: T[], n: number, comparator: (a: T, b: T) => number): T[]
+private static PartialQuickSort<T>(a: T[], n: number, comparator: (a: T, b: T) => number, low: number, high: number): T[]
+private static PartialQuickSort<T>(a: T[], n: number, comparator: (a: T, b: T) => number, low: number = 0, high: number = a.length - 1): T[]
+{
+	const partition = (a: T[], low: number, high: number) =>
+	{
+		const pivot = a[high];
+		let i = low - 1;
+
+		for (let j = low; j <= high - 1; j++)
+		{
+			const compare = comparator(a[j], pivot);
+			if (compare <= 0) // if a[j] <= pivot
+			{
+				i++;
+				const temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+
+		const temp = a[i+1];
+		a[i+1] = a[high];
+		a[high] = temp;
+
+		return i + 1;
+	}
+
+	if (low < high)
+	{
+		const partitionIndex = partition(a, low, high);
+		this.PartialQuickSort(a, n, comparator, low, partitionIndex - 1);
+
+		if (partitionIndex < n-1)
+			this.PartialQuickSort(a, n, comparator, partitionIndex + 1, high);
+	}
+
+	return a;
+}
